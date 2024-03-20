88163a00eef72ea7ff51b17cc545732b77ce3837
RiveraHan <hanzellrivera95@gmail.com>
2024-03-19 22:40:35 -0600

    more info

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const os = require('node:os')
 
 const infoMachine = {
+    'HOSTNAME': os.hostname(),
     'OSNAME': os.platform(),
     'VERSION': os.version(),
     'ARCHITECTURY': os.arch(),
@@ -8,7 +9,8 @@ const infoMachine = {
     'MEMORY': os.freemem() / 1024 / 1024,
     'ALLMEMORY': os.totalmem() / 1024 / 1024,
     'USER': os.userInfo(),
-    'UPTIME': os.uptime() / 60 / 60
+    'UPTIME': os.uptime() / 60 / 60,
+    'TYPE': os.type(),
 }
 
 console.log(infoMachine)
